552aca4190955451b4b075c2085620f234a8495e
mcflugen <mcflugen@gmail.com>
2017-04-16 13:28:05 -0600

    Initial conde import.

---
+++ README.rst
@@ -0,0 +1,35 @@
+============
+landlab REST
+============
+
+A RESTful interface to landlab graphs.
+
+
+----------
+Quickstart
+----------
+
+Use `conda` to install the necessary requirements,
+
+    $ conda install --file=requirements.txt -c conda-forge -c landlab
+
+Start the server,
+
+    $ python ./server.py
+
+Look at the line containing `Serving on` to see what host and port the
+server is running on. Alternatively, you can use the `--host` and `--port`
+options to specify a specific host and port (`--help` for help).
+
+Now you should be able to send requests to the server. For instance,
+to get a `RasterModelGrid`,
+
+    $ curl https://0.0.0.0:8080/graphs/raster
+
+For a list of supported graphs
+
+    $ curl https://0.0.0.0:8080/graphs/
+
+You can pass parameters like,
+
+    $ curl 'https://0.0.0.0:8080/graphs/raster?shape=4,5&spacing=2.,1.'
---
+++ server.py
@@ -0,0 +1,52 @@
+from landlab_rest import create_app
+
+
+app = create_app()
+
+
+# Import CherryPy
+import cherrypy
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--port', type=int, default=80,
+                        help='port to run on')
+    parser.add_argument('--host', type=str, default='0.0.0.0',
+                        help='host IP address')
+
+    args = parser.parse_args()
+
+    # Mount the application
+    cherrypy.tree.graft(app, "/")
+
+    # Unsubscribe the default server
+    cherrypy.server.unsubscribe()
+
+    # Instantiate a new server object
+    server = cherrypy._cpserver.Server()
+
+    # Configure the server object
+    server.socket_host = args.host
+    server.socket_port = args.port
+    server.thread_pool = 30
+
+    # For SSL Support
+    # server.ssl_module            = 'pyopenssl'
+    # server.ssl_certificate       = 'ssl/certificate.crt'
+    # server.ssl_private_key       = 'ssl/private.key'
+    # server.ssl_certificate_chain = 'ssl/bundle.crt'
+
+    # Subscribe this server
+    server.subscribe()
+
+    # Start the server engine (Option 1 *and* 2)
+
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+
+
+if __name__ == '__main__':
+    main()
