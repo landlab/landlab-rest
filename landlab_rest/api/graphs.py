e0ba7177c792454c779107761a107fab79259111
Greg Tucker <gtucker@colorado.edu>
2017-06-18 14:36:32 -0600

    add raster graph w corners and faces

--- landlab_rest/api/graphs.py
+++ landlab_rest/api/graphs.py
@@ -31,27 +31,37 @@ def jsonify_collection(items):
                     mimetype='application/x-collection+json; charset=utf-8')
 
 
-def to_resource(grid, href=None):
+def to_resource(grid, graph, href=None):
     return {
         '_type': 'graph',
         'href': href,
-        'graph': grid_as_dict(grid),
+        'graph': grid_as_dict(grid, graph),
     }
 
 
-def grid_as_dict(grid):
+def grid_as_dict(grid, graph):
     nodes_at_link = np.vstack((grid.node_at_link_tail,
                                grid.node_at_link_head)).T
+    corners_at_face = np.vstack((graph.corner_at_face_tail,
+                                 graph.corner_at_face_head)).T
 
     dataset = xr.Dataset({
         'y_of_node': xr.DataArray(grid.y_of_node, dims=('node', )),
         'x_of_node': xr.DataArray(grid.x_of_node, dims=('node', )),
+        'y_of_corner': xr.DataArray(graph.y_of_corner, dims=('corner', )),
+        'x_of_corner': xr.DataArray(graph.x_of_corner, dims=('corner', )),
         'y_of_link': xr.DataArray(grid.y_of_link, dims=('link', )),
         'x_of_link': xr.DataArray(grid.x_of_link, dims=('link', )),
+        'y_of_face': xr.DataArray(grid.y_of_face, dims=('face', )),
+        'x_of_face': xr.DataArray(grid.x_of_face, dims=('face', )),
         'nodes_at_link': xr.DataArray(nodes_at_link,
                                       dims=('link', 'nodes_per_link', )),
+        'corners_at_face': xr.DataArray(corners_at_face,
+                                      dims=('face', 'corners_per_face', )),
         'nodes_at_patch': xr.DataArray(grid.nodes_at_patch,
                                        dims=('patch', 'nodes_per_patch', )),
+        'corners_at_cell': xr.DataArray(graph.corners_at_cell,
+                                        dims=('cell', 'corners_per_cell', )),
     })
 
     return dataset.to_dict()
@@ -76,8 +86,10 @@ def raster():
     spacing = [float(n) for n in args['spacing'].split(',')]
 
     grid = landlab.RasterModelGrid(shape, spacing=spacing)
+    graph = landlab.graph.DualUniformRectilinearGraph(shape,
+                                                      spacing=spacing)
     return as_resource(to_resource(
-        grid,
+        grid, graph,
         href='/graph/raster?{params}'.format(params=urllib.urlencode(args))))
 
 
