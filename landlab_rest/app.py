51f6c9bbb5c3e11c946e0ea38adf5ae5e924f10f
mcflugen <mcflugen@gmail.com>
2019-04-17 12:17:48 -0600

    style: format for black

--- landlab_rest/app.py
+++ landlab_rest/app.py
@@ -1,18 +1,17 @@
 import importlib
 
-from flask import Flask, jsonify, url_for, Blueprint
+from flask import Blueprint, Flask, jsonify, url_for
 
 
 def register_blueprints(app):
     rv = []
 
-    for name in ['graphs', ]:
-        m = importlib.import_module('.api.{bp}'.format(bp=name),
-                                    package='landlab_rest')
+    for name in ["graphs"]:
+        m = importlib.import_module(".api.{bp}".format(bp=name), package="landlab_rest")
         for item in dir(m):
             item = getattr(m, item)
             if isinstance(item, Blueprint):
-                app.register_blueprint(item, url_prefix='/' + item.name)
+                app.register_blueprint(item, url_prefix="/" + item.name)
             rv.append(item)
 
     return rv
@@ -21,16 +20,16 @@ def register_blueprints(app):
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
-    @app.route('/')
+    @app.route("/")
     def site_map():
-        COLLECTIONS = ['graphs', ]
+        COLLECTIONS = ["graphs"]
 
-        map ={'@type': 'api','href': url_for('.site_map')}
+        map = {"@type": "api", "href": url_for(".site_map")}
         links = []
         for rel in COLLECTIONS:
-            href = url_for('.'.join([rel, 'show']))
-            links.append({'rel': rel, 'href': href})
-        map['links'] = links
+            href = url_for(".".join([rel, "show"]))
+            links.append({"rel": rel, "href": href})
+        map["links"] = links
         return jsonify(map)
 
     register_blueprints(app)
