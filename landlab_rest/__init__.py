91fcc55e5b2e06eb849d25f5a842269b47718153
mcflugen <mcflugen@gmail.com>
2022-05-09 13:34:06 -0600

    get version from _version.py

--- landlab_rest/__init__.py
+++ landlab_rest/__init__.py
@@ -1,6 +1,4 @@
-from ._version import get_versions
+from ._version import __version__
 from .app import create_app
 
 __all__ = ["create_app"]
-__version__ = get_versions()["version"]
-del get_versions
