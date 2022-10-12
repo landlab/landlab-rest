22b62075ac48618bd81f9e4ab9b771987f7cc4ea
mcflugen <mcflugen@gmail.com>
2022-10-12 10:15:03 -0600

    add intersphinx mapping for landlab

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -186,8 +186,11 @@ htmlhelp_basename = "landlab_restdoc"
 
 # -- Options for intersphinx extension ---------------------------------------
 
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"https://docs.python.org/": None}
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "landlab": ("https://landlab.readthedocs.io/en/master/", None),
+}
+
 
 # -- Options for todo extension ----------------------------------------------
 
